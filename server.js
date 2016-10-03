25f0487af1cc51120a5a1dc847dbfda80299978a
asushin <csushin1004@gmail.com>
2016-10-03 12:25:21 -0700

    update the port number for Heroku depolyment

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ io.on('connection', function(socket){
 
 
 // listen the port 3000
-http.listen(3000, function(){
-    console.log('listening on *:8080');
+http.listen((process.env.PORT || 3000), function(){
+    console.log('listening...');
 });
 
